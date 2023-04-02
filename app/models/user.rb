8bbb81b6136b73bdc32a4dead031b818c1cec9f2
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-02 22:51:23 +0300

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :likes
+    has_many :bookings, dependent: :destroy
+    has_many :events ,through: :bookings
+
+     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+     validates :password, presence: true
+     validates :first_name, presence: true
+     validates :last_name, presence: true
+     validates :phone, presence: true
+    validates :level, presence: true
 end
