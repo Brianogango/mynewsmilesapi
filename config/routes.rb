91f6ceb779ca5e299e5b267e49b76105ecd8f87a
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-02 21:59:42 +0300

    session routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
  get '/', to: 'users#index'
  post '/users', to: 'users#create'
  get '/users/:id', to: 'users#show'
- post '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+ patch '/users/:id', to: 'users#update'
+delete '/users/:id', to: 'users#destroy'
+# session
+    post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
