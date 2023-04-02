85db59c3d2f1cc002514dd85a01d249efd10a088
brianonyango <brian.ogango@student.moringaschool.com>
2023-04-02 20:15:53 +0300

    post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,29 +16,17 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
+ 
 
   # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        user = User.create(user_params)
+        if user.valid?
+            render json: { "success": "User saved successfully!"}, status: :created
+        else
+            render json: {"errors": ["Validation errors"]}, status: :unprocessable_entity
+        end
     end
-  end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
  get '/', to: 'users#index'
+ post '/users', to: 'users#create'
 end
